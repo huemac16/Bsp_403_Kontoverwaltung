f9fb1310b478151c350426677a003d03825a4929
huemac16 <huemac16@htl-kaindorf.ac.at>
2019-04-11 08:34:09 +0200

    added gui and konto class

---
+++ src/main/java/bl/Konto.java
@@ -0,0 +1,28 @@
+package bl;
+
+public class Konto {
+
+    private int balance;
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public Konto(int balance) {
+        this.balance = balance;
+    }
+
+    public void withdraw(int amount) {
+        balance -= amount;
+    }
+
+    public void deposit(int amount) {
+        balance += amount;
+
+    }
+
+}
